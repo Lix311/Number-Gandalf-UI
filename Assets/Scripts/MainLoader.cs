707d5db6054528b84d8c77883b9da21a2ef1a84a
Lix311 <alexsterrantino@gmail.com>
2020-11-10 18:04:39 -0500

    scene functions added

---
+++ Assets/Scripts/MainLoader.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainLoader : MonoBehaviour
+{
+    // Start is called before the first frame update
+    
+        
+    public void LoadMainScene(){
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(0);
+    }
+}
