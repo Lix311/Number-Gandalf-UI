38140b62713df03638636e080026a706a0f41562
Lix311 <alexsterrantino@gmail.com>
2020-11-14 22:37:50 -0500

    added higher lower button logic

--- Assets/Scripts/NumberWizard.cs
+++ Assets/Scripts/NumberWizard.cs
@@ -1,11 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class NumberWizard : MonoBehaviour
 {
     [SerializeField] int max;
     [SerializeField] int min;
+    [SerializeField] TextMeshProUGUI guessText;
+    
     int guess;  
     // Start is called before the first frame update
     void Start()
@@ -14,25 +17,29 @@ public class NumberWizard : MonoBehaviour
     }
 
     void StartGame(){
-        max = max + 1;
-        guess = (max + min) / 2;
        
+        guess = (max + min) / 2;
+        guessText.text = guess.ToString();
+        max = max + 1;
     }
 
     // Update is called once per frame
     public void OnPressHigher()
     {
+       
         min = guess;
         NextGuess();
     }
 
     public void OnPressLower()
     {
+        
         max = guess;
         NextGuess();
     }
 
     void NextGuess(){
          guess = (max + min) / 2;
+         guessText.text = guess.ToString();
     }
 }
